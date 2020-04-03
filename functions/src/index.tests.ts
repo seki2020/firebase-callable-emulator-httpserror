53b9b29f8166a845ba017582630a1457482d82d9
xi <ming.xi@logical-fabrics.com>
2020-04-03 11:40:05 +0800

    set real projectId

--- functions/src/index.tests.ts
+++ functions/src/index.tests.ts
@@ -1,15 +1,15 @@
-import test from 'ava';
-import * as fbtest from '@firebase/testing';
+import test from "ava";
+import * as fbtest from "@firebase/testing";
 
 const testApp = fbtest.initializeTestApp({
-  projectId: "testproject-dbefa",
+  projectId: "codelab-emulator-testing",
   // If we comment the following line, it works!
   auth: { uid: "alice", email: "alice@example.com" }
 });
 const testFunctions = testApp.functions();
-testFunctions.useFunctionsEmulator("http://localhost:5001")
+testFunctions.useFunctionsEmulator("http://localhost:5001");
 
-test('we are able to call the callable function', async t => {
+test("we are able to call the callable function", async t => {
   const result = await testFunctions.httpsCallable("helloCallable")({});
   t.deepEqual(result.data, { hello: "world" });
 });
